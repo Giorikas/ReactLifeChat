10bb6266261e056336b8a4c2b3e7b0541a483098
Giorikas <gio@Duke_Lefteri.localdomain>
2021-01-02 16:12:52 +0100

    connected client to server

---
+++ client/src/Chat.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  useQuery,
+  gql,
+} from "@apollo/client";
+
+//GUI
+import {
+    Container
+} from "shards-react";
+
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+const GET_MESSAGES = gql`
+    query{
+        messages{
+            id
+            content
+            user
+        }
+    }
+`;
+
+const Messages = ({user}) => {
+    const { data } = useQuery(GET_MESSAGES);
+    if (!data) {
+        return null;
+    }
+
+    return (
+        <>
+            {data.messages.map(({id, user: messageUser, content}) =>(
+                <div
+                    style = {{
+                        display: 'flex',
+                        justifyContent: user === messageUser ? 'flex-end':'flex-start',
+                        paddingBottom : "3em",
+                    }}  
+                >    
+                    <div
+                        style = {{
+                            background: user === messageUser ? "#58bf56" : "#e5e6ea",
+                            color: user === messageUser ? "yellow" : "navy",
+                            padding: "1em",
+                            borderRadius: "1em",
+                            maxWidth: "55%",
+                        }}
+                    >
+                        {content}
+                    </div>
+                </div>
+            ))}
+        </>    
+    );
+}
+
+const Chat = () => {
+  return (
+    <Container>
+        <Messages user="Giorikas" />
+    </Container>
+  );
+};
+
+export default () => (
+  <ApolloProvider client={client}>
+    <Chat />
+  </ApolloProvider>
+);
